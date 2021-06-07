4f73532c4c58dc20211db34f33c6089a03801d04
Guiglaz <guilhem.glaziou@gmail.com>
2021-06-07 18:05:34 +0200

    hyper_viz modif

--- hyper_viz.py
+++ hyper_viz.py
@@ -4,12 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 directory = "runs"
+show = False
+Bestfit =-1
+BestParams = []
+iteration = 0
 
 for run in os.listdir(directory):
+    iteration+=1
     file = os.path.join(directory, run)
     arr = np.load(file)
     params = arr[1: int(arr[0])+1]
     fitnesses = arr[int(arr[0])+1:]
-    plt.plot(fitnesses)
-    plt.title(f"{run} - {params}")
-    plt.show()
+    if np.amax(fitnesses) > Bestfit:
+        Bestfit = np.amax(fitnesses)
+        BestParams = params
+        BestIteration = iteration
+    if show:
+        plt.plot(fitnesses)
+        plt.draw()
+        plt.pause(1)
+        plt.clf()
+
+np. set_printoptions(suppress=True)
+print("Meilleure fitness au set {} : {}".format(BestIteration, Bestfit))
+print(BestParams)
